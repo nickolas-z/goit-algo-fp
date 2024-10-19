09a6260175e24a741cd4b7b230e42efae07923f6
Mykola Zubchyk <nickolasz@gmail.com>
2024-10-19 15:35:07 +0300

    Implement task1

--- task1.py
+++ task1.py
@@ -1,83 +1,75 @@
-def reverse(self):
-    prev = None
-    current = self.head
-    while current:
-        next_node = current.next
-        current.next = prev
-        prev = current
-        current = next_node
-    self.head = prev
+from LinkedList import LinkedList
 
-def sorted_insert(self, new_node: Node):
-    if self.head is None or self.head.data >= new_node.data:
-        new_node.next = self.head
-        self.head = new_node
-    else:
-        current = self.head
-        while current.next and current.next.data < new_node.data:
-            current = current.next
-        new_node.next = current.next
-        current.next = new_node
+class LList(LinkedList):
+    def sort_list(self):
+        if self.head is None:
+            return
+        cur = self.head
+        while cur:
+            min_node = cur
+            next_node = cur.next
+            while next_node:
+                if min_node.data > next_node.data:
+                    min_node = next_node
+                next_node = next_node.next
+            cur.data, min_node.data = min_node.data, cur.data
+            cur = cur.next
 
-def insertion_sort(self):
-    sorted_list = None
-    current = self.head
-    while current:
-        next_node = current.next
-        current.next = None
-        if sorted_list is None:
-            sorted_list = current
-        else:
-            self.head = sorted_list
-            self.sorted_insert(current)
-            sorted_list = self.head
-            current = next_node
-    self.head = sorted_list
-def merge_sorted_lists(l1: LinkedList, l2: LinkedList) -> LinkedList:
-    dummy = Node(0)
-    tail = dummy
+    def merge_sorted_lists(self, ll2: LinkedList) -> LinkedList:
+        merged = LList()
+        cur1 = self.head
+        cur2 = ll2.head
+        while cur1 and cur2:
+            if cur1.data < cur2.data:
+                merged.insert_at_end(cur1.data)
+                cur1 = cur1.next
+            else:
+                merged.insert_at_end(cur2.data)
+                cur2 = cur2.next
+        while cur1:
+            merged.insert_at_end(cur1.data)
+            cur1 = cur1.next
+        while cur2:
+            merged.insert_at_end(cur2.data)
+            cur2 = cur2.next
+        return merged
 
-    a = l1.head
-    b = l2.head
+    def reverse(self):
+        prev = None
+        cur = self.head
+        while cur:
+            next_node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_node
+        self.head = prev
 
-    while a and b:
-        if a.data < b.data:
-            tail.next = a
-            a = a.next
-        else:
-            tail.next = b
-            b = b.next
-        tail = tail.next
+def main():
+    # Приклад використання
+    ll1 = LList()
+    ll1.insert_at_end(3)
+    ll1.insert_at_end(1)
+    ll1.insert_at_end(5)
 
-    if a:
-        tail.next = a
-    elif b:
-        tail.next   
-    merged_list = LinkedList()
-    merged_list.head = dummy.next
-    return merged_list
+    ll2 = LinkedList()
+    ll2.insert_at_end(2)
+    ll2.insert_at_end(4)
+    ll2.insert_at_end(6)
 
-# Реверсування списку
-llist.reverse()
-print("\nРеверсований список:")
-llist.print_list()
+    print("List 1 before sorting:")
+    ll1.print_list()
 
-# Сортування списку
-llist.insertion_sort()
-print("\nВідсортований список:")
-llist.print_list()
+    ll1.sort_list()
+    print("List 1 after sorting:")
+    ll1.print_list()
 
-# Об'єднання двох відсортованих списків
-llist1 = LinkedList()
-llist1.insert_at_end(1)
-llist1.insert_at_end(3)
-llist1.insert_at_end(5)
+    print("Merged list:")
+    merged = ll1.merge_sorted_lists(ll2)
+    merged.print_list()
 
-llist2 = LinkedList()
-llist2.insert_at_end(2)
-llist2.insert_at_end(4)
-llist2.insert_at_end(6)
+    print("Reversed merged list:")
+    merged.reverse()
+    merged.print_list()
 
-merged_list = merge_sorted_lists(llist1, llist2)
-print("\nОб'єднаний відсортований список:")
-merged_list.print_list()
+if __name__ == "__main__":
+    main()
